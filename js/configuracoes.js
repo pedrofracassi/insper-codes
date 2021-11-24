2b6bfb5b4ffb557b75d33743f2f066f5d967c88d
jprl123 <joaoprl1@al.insper.edu.br>
2021-11-24 09:36:37 -0300

    config inicio

---
+++ js/configuracoes.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // use the class section6 to click and add 
+    var novo = document.querySelector('.section6');
+    novo.addEventListener('click', function() {
+    }
+    );
+    
+
+
+});
