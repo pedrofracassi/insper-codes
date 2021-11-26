2b35bfc6ef00cd1335b46e895b2038ed65830ad9
jprl123 <joaoprl1@al.insper.edu.br>
2021-11-26 10:57:15 -0300

    Botão de configuração

--- js/configuracoes.js
+++ js/configuracoes.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // use the class section6 to click and add 
-    var novo = document.querySelector('.section6');
-    novo.addEventListener('click', function() {
-    }
-    );
-    
-
-
+    input = document.querySelector('input');
+    button = document.querySelector(".adicionar");
+    button.addEventListener("click", function() {
+        value = input.value;
+        const bolha = 
+        `<h2 class='summary'>
+            ${value}
+          </h2>`;	
+        document.getElementById("salao").innerHTML += bolha;
+        input.value = "";
+    });
 });
